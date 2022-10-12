01f5d8df8f0b95f886e1d546d60284333a15ff5f
ayukomakotu <yamamesan708@gmail.com>
2022-10-12 22:52:03 +0900

    nodeをダウングレード　互換性がなく動かないため

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,13 @@
+// function xxx() は Ruby の def のようなものです
+function foo() {
+    x = 'foo'; // 一時変数を宣言
+    alert(x);
+}
+
+foo(); // 先ほど宣言した function を実行
+
+// function bar() {
+//     alert(x);
+// }
+
+// bar();
